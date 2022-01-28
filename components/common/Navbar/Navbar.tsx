95125d3b030fc3b10bf66bb1547776566bebb303
thienjs <thien.tran6@gmail.com>
2022-01-27 23:51:53 -0500

    search toggle

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -23,55 +23,57 @@ const Navbar: FC<NavbarProps> = ({ links }) => (
   <NavbarRoot>
     <Container>
       <Disclosure>
-
-        <div className={s.nav}>
-          <div className="flex items-center flex-1">
-            <Link href="/">
-              <a className={s.logo} aria-label="Nice">
-                <Logo />
-              </a>
-            </Link>
-            <Link href="/">
-              <a className="" aria-label="Home">
-                <NiceLogo className="" />
-              </a>
-            </Link>
-
-            <nav className={s.navMenu}>
-              <Link href="/search">
-                <a className={s.link}>All</a>
-              </Link>
-              {links?.map((l) => (
-                <Link href={l.href} key={l.href}>
-                  <a className={s.link}>{l.label}</a>
+        {({ open }) => (
+          <div>
+            <div className={s.nav}>
+              <div className="flex items-center flex-1">
+                <Link href="/">
+                  <a className={s.logo} aria-label="Nice">
+                    <Logo />
+                  </a>
                 </Link>
-              ))}
-            </nav>
-          </div>
-          {process.env.COMMERCE_SEARCH_ENABLED && (
-            <div className="justify-center flex-1 hidden lg:flex mt-5">
-              <Searchbar />
-            </div>
-          )}
+                <Link href="/">
+                  <a className="" aria-label="Home">
+                    <NiceLogo className="" />
+                  </a>
+                </Link>
 
-          <div className="flex items-center justify-end flex-1 space-x-2">
-            <Disclosure.Button className="lg:hidden">
-              <FiSearch className='' size={20} />
-            </Disclosure.Button>
-            <ThemeSwitch />
-            <UserNav />
-          </div>
-        </div>
-        <div>
-          {process.env.COMMERCE_SEARCH_ENABLED && (
-            <div>
-              <Disclosure.Panel className="pb-2">
-                <Searchbar id="mobile-search" />
-              </Disclosure.Panel>
+                <nav className={s.navMenu}>
+                  <Link href="/search">
+                    <a className={s.link}>All</a>
+                  </Link>
+                  {links?.map((l) => (
+                    <Link href={l.href} key={l.href}>
+                      <a className={s.link}>{l.label}</a>
+                    </Link>
+                  ))}
+                </nav>
+              </div>
+              {process.env.COMMERCE_SEARCH_ENABLED && (
+                <div className="justify-center flex-1 hidden lg:flex mt-5">
+                  <Searchbar />
+                </div>
+              )}
+
+              <div className="flex items-center justify-end flex-1 space-x-2">
+                <Disclosure.Button className="lg:hidden">
+                  <FiSearch className={`${open ? "transform rotate-90" : ""}`} size={20} />
+                </Disclosure.Button>
+                <ThemeSwitch />
+                <UserNav />
+              </div>
             </div>
-          )}
-        </div>
-         
+            <div>
+              {process.env.COMMERCE_SEARCH_ENABLED && (
+                <div>
+                  <Disclosure.Panel className="pb-2 lg:hidden">
+                    <Searchbar id="mobile-search" />
+                  </Disclosure.Panel>
+                </div>
+              )}
+            </div>
+          </div>
+        )}
       </Disclosure>
     </Container>
   </NavbarRoot>
