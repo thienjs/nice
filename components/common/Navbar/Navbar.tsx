d67ee3d3c88c65e5c10d912b0ca43729ab106df4
thienjs <thien.tran6@gmail.com>
2022-01-27 23:45:52 -0500

    search

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -7,6 +7,8 @@ import { Searchbar, UserNav } from '@components/common'
 import ThemeSwitch from './ThemeSwitch'
 import { ThemeProvider } from 'next-themes'
 import NiceLogo from './NiceLogo'
+import { Disclosure } from '@headlessui/react'
+import { FiSearch } from 'react-icons/fi'
 
 interface Link {
   href: string
@@ -20,45 +22,57 @@ interface NavbarProps {
 const Navbar: FC<NavbarProps> = ({ links }) => (
   <NavbarRoot>
     <Container>
-      <div className={s.nav}>
-        <div className="flex items-center flex-1">
-          <Link href="/">
-            <a className={s.logo} aria-label="Nice">
-              <Logo />
-            </a>
-          </Link>
-          <Link href="/">
-            <a className="" aria-label="Home">
-              <NiceLogo className='' />
-            </a>
-          </Link>
+      <Disclosure>
 
-          <nav className={s.navMenu}>
-            <Link href="/search">
-              <a className={s.link}>All</a>
+        <div className={s.nav}>
+          <div className="flex items-center flex-1">
+            <Link href="/">
+              <a className={s.logo} aria-label="Nice">
+                <Logo />
+              </a>
             </Link>
-            {links?.map((l) => (
-              <Link href={l.href} key={l.href}>
-                <a className={s.link}>{l.label}</a>
+            <Link href="/">
+              <a className="" aria-label="Home">
+                <NiceLogo className="" />
+              </a>
+            </Link>
+
+            <nav className={s.navMenu}>
+              <Link href="/search">
+                <a className={s.link}>All</a>
               </Link>
-            ))}
-          </nav>
-        </div>
-        {process.env.COMMERCE_SEARCH_ENABLED && (
-          <div className="justify-center flex-1 hidden lg:flex mt-5">
-            <Searchbar />
+              {links?.map((l) => (
+                <Link href={l.href} key={l.href}>
+                  <a className={s.link}>{l.label}</a>
+                </Link>
+              ))}
+            </nav>
           </div>
-        )}
-        <div className="flex items-center justify-end flex-1 space-x-3">
-          <ThemeSwitch />
-          <UserNav />
+          {process.env.COMMERCE_SEARCH_ENABLED && (
+            <div className="justify-center flex-1 hidden lg:flex mt-5">
+              <Searchbar />
+            </div>
+          )}
+
+          <div className="flex items-center justify-end flex-1 space-x-2">
+            <Disclosure.Button className="lg:hidden">
+              <FiSearch className='' size={20} />
+            </Disclosure.Button>
+            <ThemeSwitch />
+            <UserNav />
+          </div>
         </div>
-      </div>
-      {process.env.COMMERCE_SEARCH_ENABLED && (
-        <div className="flex pb-4 lg:px-6 lg:hidden">
-          <Searchbar id="mobile-search" />
+        <div>
+          {process.env.COMMERCE_SEARCH_ENABLED && (
+            <div>
+              <Disclosure.Panel className="pb-2">
+                <Searchbar id="mobile-search" />
+              </Disclosure.Panel>
+            </div>
+          )}
         </div>
-      )}
+         
+      </Disclosure>
     </Container>
   </NavbarRoot>
 )
