83f982a4f8ee6c1499222a855df64a6c81b1651b
thienjs <thienjsx@gmail.com>
2022-01-28 23:46:41 -0500

    carousel update

--- components/common/Carousel/Carousel.tsx
+++ components/common/Carousel/Carousel.tsx
@@ -1,23 +1,44 @@
-import React from 'react';
+import { FC, useState } from 'react'
+import type { Product } from '@commerce/types/product'
+import Image, { ImageProps } from 'next/image'
 
 interface CarouselProps {
-  img:string
+  product: Product
+  imgProps?: Omit<ImageProps, 'src' | 'layout' | 'placeholder' | 'blurDataURL'>
 }
 
-
-function Carousel(props:CarouselProps) {
+const Carousel: FC<CarouselProps> = ({ product, imgProps }) => {
+  const placeholderImg = '/product-img-placeholder.svg'
+  let count = 0
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const handleOnNextClick = () => {
+    count = (count + 1) % product.images.length
+    setCurrentIndex(count)
+  }
+  const handleOnPrevClick = () => {
+    const productsLength = product.images.length
+    count = (currentIndex + productsLength - 1) % productsLength
+    setCurrentIndex(count)
+  }
   return (
-    <div className='max-w-screen-xl mx-auto'>
-      <div className='w-full relative select-none'>
-        <div className='aspect-w-16'>
-
+    <div className="max-w-screen-xl mx-auto">
+      <div className="w-full relative select-none">
+        <div className="aspect-w-16 aspect-h-16">
+          {product?.images && (
+            
+              <Image
+                quality="100"
+                src={product.images[0]?.url || placeholderImg}
+                alt={product.name || 'Product Image'}
+                layout="fill"
+                {...imgProps}
+              />
+            
+          )}
         </div>
       </div>
-
     </div>
-  );
+  )
 }
 
-
-
-export default Carousel;
+export default Carousel
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import commerce from '@lib/api/commerce'
-import { Layout } from '@components/common'
+import { Layout, Carousel } from '@components/common'
 import { ProductCard } from '@components/product'
 import { Grid, Marquee, Hero } from '@components/ui'
 // import HomeAllProductsGrid from '@components/common/HomeAllProductsGrid'
@@ -39,23 +39,23 @@ export default function Home({
   products,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <div className=''>
+    <div className="">
       <Hero
         headline="Nice Co"
         description="We strive to give beauty professionals the highest quality products at the best prices.  "
       />
+      {products.slice(0, 5).map((product: any, i: number) => (
+      <Carousel product={product}/>
+      ))}
+      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="">Products</h2>
 
-
-        <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-          <h2 className="">Products</h2>
-
-          <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-            {products.slice(0, 15).map((product: any, i: number) => (
-               <ProductCard key={product.id} product={product} variant="modern" />
-            ))}
-          </div>
+        <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          {products.slice(13, 29).map((product: any, i: number) => (
+            <ProductCard key={product.id} product={product} variant="modern" />
+          ))}
         </div>
-     
+      </div>
 
       <Marquee variant="secondary">
         {products.slice(16, 27).map((product: any, i: number) => (
